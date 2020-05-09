7e2021041f7488e0924b887d7813acbe296950e5
raghav <raghav.81-cse-17@mietjammu.in>
2020-05-09 12:37:22 +0530

    test3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 let body=document.body;
-body.addEventListener("mousemove",()=>{
-body.style.backgroundColor=`${e.offsetX},${e.offsetY},54`)
+body.addEventListener("mousemove",(e)=>{
+body.style.backgroundColor=`${e.offsetX},${e.offsetY},54`
 });
