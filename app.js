36186c84596652270a1f2e5da2b64a7853ea27a3
raghav <raghav.81-cse-17@mietjammu.in>
2020-05-09 12:08:02 +0530

    testdev

---
+++ app.js
@@ -0,0 +1,5 @@
+let body=document.body;
+body.addEventListener("mousemove",()=>{
+}[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cbody.[D[D[D[D[D[D[D[D[D[D[Ce[C[C[C[D[D[D)=>{body.style.background=`rgb(e.offsetX,e.offsetY,54});[D[D[D)
+[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C
+});
